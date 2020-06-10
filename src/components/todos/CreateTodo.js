5ee6840186a4ce7e212963839eaa2766184138d8
github username <github email address>
2020-06-10 13:13:40 -0700

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -21,6 +21,15 @@ class CreateTodo extends Component {
     });
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+    this.setState({
+      text: '',
+    })
+  }
+  
+
   render() {
     return(
       <div>
